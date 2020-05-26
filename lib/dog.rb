76551279a24f320856130e123ee989a931cb9302
Olivia Hanson <olivia.joy.hanson@gmail.com>
2020-05-26 22:23:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,13 @@
 class Dog
   attr_accessor :name
   @@all = []
+  def self.save
+    @@all << self
+  end
 
   def initialize(name)
     @name = name
     self.save
-  end
-
-  def self.save
-    @@all << self
   end
 
   def self.all
