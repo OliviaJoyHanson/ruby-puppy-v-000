504f72f7f5d3e965fedab58b36075e474e58e315
Olivia Hanson <olivia.joy.hanson@gmail.com>
2020-05-26 22:00:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,21 @@
 class Dog
-
+  attr_reader :name
   @@all = []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.print_all
+    @@all.each{|dog| puts "#{dog.name}"}
+  end
 
 end
